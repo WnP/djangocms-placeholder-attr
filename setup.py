22553f0739180571355afe9df4bc43a509a3faee
Steeve <steevechailloux@gmail.com>
2014-10-03 16:38:13 +0200

    change license to MIT, update setup

--- setup.py
+++ setup.py
@@ -2,22 +2,33 @@
 from setuptools import setup, find_packages
 
 
+try:
+    import pypandoc
+    description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    with open('README.md', 'rb') as f:
+        description = f.read()
+
+
 setup(
     name='djangocms-placeholder-attr',
-    version='0.0.1',
+    version='1.0.1b',
+    license='MIT',
     description='Allow you to call a plugin model attribute from a placeholder in django-cms',
+    long_description=description,
     author='Steeve',
     author_email='mo0ofier@gmail.com',
     include_package_data=True,
-    url='https://github.com/WnP/djangocms-css-background',
+    url='https://github.com/WnP/djangocms-placeholder-attr',
     packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
     ],
     zip_safe=False,
 )
